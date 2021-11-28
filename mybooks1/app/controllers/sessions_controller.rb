82be3d04cba53b17dc91b6315f254e6e8f05345f
SoNesky <gueronlj@gmail.com>
2021-11-27 19:38:09 -0500

    fixed login

--- mybooks1/app/controllers/sessions_controller.rb
+++ mybooks1/app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+     @user = User.find_by(username: params[:user][:username])
+
+     if @user.authenticate(params[:user][:password])
+        session[:current_user_id] = @user.id
+        redirect_to @user
+
+     else
+        flash[:message] = @user.errors.full_messages.to_sentence
+        redirect_to new_session_path
+     end
+  end
+
+  def destroy
+
+  end
 end
--- mybooks1/app/controllers/users_controller.rb
+++ mybooks1/app/controllers/users_controller.rb
@@ -1,19 +1,42 @@
 class UsersController < ApplicationController
+   before_action :find_user, only: [:edit, :show, :update]#use teh find_user function before hitting these select routes
+   before_action :find_current_user
+
+   def index
+      @users = User.all
+
+      respond_to do |format|
+         format.html { render 'index'}#if request format is html show regular.
+         format.json { render json:  @users}#if request format is json, show as json object
+      end
+   end
+
    def create
       @user = User.new(user_params)
 
       if @user.save
-         redirect_to @user
+         redirect_to users_path
       else
          flash[:message] = @user.errors.full_messages.to_sentence
          redirect_to new_user_path
       end
    end
 
+   def show
+   end
 
    private
 
    def user_params
-      params.require(:user).permit(:username, :password, :password_confirm)
+      params.require(:user).permit(:username, :password)
+   end
+
+   def find_user
+      @user = User.find(params[:id])#put this in a function so we dont have to write in every route. MORE DRY
+   end
+
+   def find_current_user
+      @current_user = User.find(session[:current_user_id]) if
+      session[:current_user_id]#If something is stored in current_user_id, use it to look up a user
    end
 end
