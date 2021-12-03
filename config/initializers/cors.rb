07e2f97f04e0390fbaea3580446d3cdfd712d273
SoNesky <gueronlj@gmail.com>
2021-12-03 01:02:45 -0500

    trying to fix cors error on login

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
-    resource '/session', headers: :any, methods: [:get, :post, :show]
+    resource '*', headers: :any, methods: [:get, :post, :show, :head, :delete, :options, :put, :patch]
   end
 end
