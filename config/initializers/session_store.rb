fc402a36714585a3e3412387035a949a72f4c057
SoNesky <gueronlj@gmail.com>
2021-12-03 12:13:00 -0500

    trying to fix cors error on login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
   Rails.application.config.session_store :cookie_store, key: '_mybooks-frontend', domain: 'https://protected-eyrie-39175.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_mybooks-frontend'
+  Rails.application.config.session_store :cookie_store, key: '_mybooks-frontend', domain: 'http://localhost:3000'
 end
