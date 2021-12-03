3afc9235f8dd07a6becd70805dc62d0d0fd4345d
SoNesky <gueronlj@gmail.com>
2021-12-03 13:09:30 -0500

    trying to make login session real

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def show
+     @user = User.find_by(id: session[:current_user_id])
      render json: {current_user: current_user}
   end
 
   def create
      @user = User.find_by(username: user_params[:username])
      if @user.authenticate(user_params[:password])
-        login!(@user)      
+        login!(@user)
      else
         flash[:message] = @user.errors.full_messages.to_sentence
         redirect_to new_session_path
