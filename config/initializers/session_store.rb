668e5224964aeedbe49ff1e8aaec902534c1d877
SoNesky <gueronlj@gmail.com>
2021-12-03 20:04:18 -0500

    trying to make login session real

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
- Rails.application.config.session_store :cookie_store, key: '_mybooks-frontend', domain: 'https://protected-eyrie-39175.herokuapp.com'
+ # Rails.application.config.session_store :cookie_store, key: '_mybooks-frontend', domain: 'https://protected-eyrie-39175.herokuapp.com'
