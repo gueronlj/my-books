1b47f2de413012b8c26613c690ab2fde5995e895
SoNesky <gueronlj@gmail.com>
2021-12-08 12:37:41 -0500

    change link in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://mybooks-frontend.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :show, :head, :delete, :options, :put, :patch], credentials: true
   end
 end
