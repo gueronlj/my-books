9049ffbc098993e747072ba321230dd0c01cfc09
SoNesky <gueronlj@gmail.com>
2021-12-08 13:26:46 -0500

    switching cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mybooks-frontend.herokuapp.com'
+    origins 'http://localhost:3001'
     resource '*', headers: :any, methods: [:get, :post, :show, :head, :delete, :options, :put, :patch], credentials: true
   end
 end
