02b14053342c7611b0ace3983b5db46ee86d4ff5
SoNesky <gueronlj@gmail.com>
2021-12-08 19:50:52 -0500

    trying to fix CORS origin URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://protected-eyrie-39175.herokuapp.com/'
+    origins "https://protected-eyrie-39175.herokuapp.com/"
     resource '*', headers: :any, methods: [:get, :post, :show, :head, :delete, :options, :put, :patch], credentials: true
   end
 end
